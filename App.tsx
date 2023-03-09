cf9b1ca6c188dabe068debfe63bcf945a3329b36
matheus jesse <matheus_jesse@hotmail.com>
2023-03-09 19:14:22 -0300

    iniciando projeto React Native

---
+++ App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>Bem vindo!</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
