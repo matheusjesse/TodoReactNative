f58f33b028b47088a59f48e79c5a0d5ee9c27fe3
matheus jesse <matheus_jesse@hotmail.com>
2023-03-16 23:09:09 -0300

    feat: criando o componente TodoForm

---
+++ src/components/TodoForm/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import DayPeriod from '../DayPeriod';
+import TodoFormContainer from './style';
+
+export default function TodoForm() {
+  return (
+    <TodoFormContainer>
+      <DayPeriod />
+    </TodoFormContainer>
+  );
+}
---
+++ src/components/TodoForm/style.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components/native';
+
+const TodoFormContainer = styled.View``;
+
+export default TodoFormContainer;
